2baaeb06c9a95ae344c5fdf8d060a08455a01a12
muhmp <defcoolz@gmail.com>
2021-12-13 00:36:17 +0900

    practice

--- CodingTest_I/separateText.js
+++ CodingTest_I/separateText.js
@@ -6,13 +6,42 @@
 
 /*1. practice*/
 
-//define 
-const txt = ["BMW",
-            "Mercy",
-            "Lambo"];
+//define object inside array
+const txt = ["BMW","Mercy","Lambo"];
 
-            //split function
-            const word = txt.split(word[1]);
-
-console.log(txt[2]);
 console.log(txt);
+
+
+/*2. using split to split text */
+
+const x = "helloworld"
+for (i=0; i<x.length; i++){
+    console.log(x.charAt(i));
+}
+
+
+
+/** Main question **/
+
+const counts = ["900, google.com", 
+                "700, Amazon.com",
+                "650, mail.yahoo.com"]
+
+function solution(N) {
+    for(i=0 ; i<counts.length; i++){
+        N = counts;
+    }
+   
+}
+ console.log(N)
+
+
+/**example 
+var sampleString = "aaaaa12111";
+
+var seperate = sampleString.split('').reduce(function(start , item){
+  Number(item) ? start[0] += item : start[1] += item;
+  return start
+},['',''])
+
+console.log(seperate) //["12111", "aaaaa"]*/
