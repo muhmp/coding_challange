cff8b13f4c124b0912b32df1a22209b7039f90a3
muhmp <defcoolz@gmail.com>
2021-12-23 23:13:56 +0900

    Create longestWord.js

---
+++ CoderByte/longestWord.js
@@ -0,0 +1,32 @@
+/**Try 1  */
+/*
+ * 1.Split
+ * 2. initialize for hold the length of the longest word
+ * 3. for loop for each string plus compare inside loop
+ 
+function LongestWord(sen) { 
+    //split
+    var strSplit = sen.split(' ');
+    //initialize hold longest word
+    var longWord = 0;
+    //loop
+    for(var i = 0 ; i<strSplit.length;i++){
+        if(strSplit[i].length > longWord){
+        longWord = strSplit[i].length;
+
+        }
+    }
+
+    //results
+    return longWord;
+
+    }
+console.log(LongestWord("hello I am human"));
+*/
+
+/**Solution 2 */
+
+
+function LongestWord(sen) { 
+
+}
