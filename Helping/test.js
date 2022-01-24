5e48421f27b5b126cdd508079b87f10dc714942f
muhmp <defcoolz@gmail.com>
2022-01-24 23:43:59 +0900

    Create test.js

---
+++ Helping/test.js
@@ -0,0 +1,11 @@
+function tanya(a,b){
+    let result = 0; //store
+    if(b==0){
+        tanya === a;
+    }else{
+       tanya = tanya*(a+(b % 10),(b-b% 10)/10);
+    }
+        return tanya 
+}
+
+console.log(tanya(1,999999));
