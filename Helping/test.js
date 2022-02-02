3b72eaac1ddee7e636c100c8f21fdf17a819b1d6
muhmp <defcoolz@gmail.com>
2022-02-02 23:24:31 +0900

    testing practice

--- Helping/test.js
+++ Helping/test.js
@@ -1,4 +1,6 @@
-function tanya(a,b){
+/* 
+
+function divisor(a,b){
     let result = 0; //store
     if(b==0){
         tanya === a;
@@ -8,4 +10,19 @@ function tanya(a,b){
         return tanya 
 }
 
-console.log(tanya(1,999999));
+console.log(tanya(1,999999)); */
+
+//create a divisor function that returns the number of times a number can be divided by another number
+//divisor(4,2) should return 2
+//divisor(9,3) should return 3
+function divisor(a,b){
+    let result = 0; //store
+    if(b==0){
+        result = a;
+    }else{
+       result = result*(a+(b % 10),(b-b% 10)/10);
+    }
+        return result 
+}
+
+console.log(divisor(1,999999));
