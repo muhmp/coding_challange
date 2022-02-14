b3ba01a0d28942c817c9ec81d97563f742483ec5
muhmp <defcoolz@gmail.com>
2022-02-14 18:11:12 +0900

    Update cdjexample.js

--- Coding Test/cdjexample.js
+++ Coding Test/cdjexample.js
@@ -7,7 +7,8 @@ function minimumCars(P, S)
                 // if value of P is less than value of S
                 if(P[i] <= S[j]){ //if [1,4,1]<=[1,5,1] 
                   //every friend had a seat in the care they came in; that is, P[K] <= S[K] for each K within the range [0..N-1]
-                   minimum = S[j];
+                  minimum = P.filter( P => P.length <=S)
+                  
                 }
             //return the length of the array
             return minimum;
