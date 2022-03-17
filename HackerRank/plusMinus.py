bc7ed506b2b4ba2a7a6e4ec4dfdbb606d1e57942
muhmp <defcoolz@gmail.com>
2022-03-17 23:22:30 +0900

    Create plusMinus.py

---
+++ HackerRank/plusMinus.py
@@ -0,0 +1,12 @@
+'''
+/**
+ # Question
+ # Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. 
+ # Print the decimal value a new line with 6 places after the decimal.
+ # 
+ # ex:
+ #  arr = [1,1,0,-1,-1]
+ #  there are 5 n elements, two positive, two negative and one zero
+ #  the ratio of positive is 2/5 = 0.4000000, negative is 2/5 = 0.4000000 and zero is 1/5 = 0.200000
+
+'''
