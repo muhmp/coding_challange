a00c5776d10b56aa2ee0cda0660154cf4feef101
muhmp <defcoolz@gmail.com>
2022-03-18 17:10:03 +0900

    Update plusMinus.py

--- HackerRank/plusMinus.py
+++ HackerRank/plusMinus.py
@@ -10,3 +10,38 @@
  #  the ratio of positive is 2/5 = 0.4000000, negative is 2/5 = 0.4000000 and zero is 1/5 = 0.200000
 
 '''
+
+# import
+import math
+import os
+import random
+import re
+import sys
+
+# function
+
+
+def plusMinus(arr):
+    x, y, z = 0, 0, 0
+    # for loop
+    for i in range(0, len(arr)):
+        if arr[i] > 0:
+            x += 1  # x += 1 // x = x + 1
+        elif arr[i] < 0:
+            y += 1
+        else:
+            z = z+1  # same
+        # print
+    print(x/len(arr))
+    print(y/len(arr))
+    print(z/len(arr))
+
+#
+
+
+if __name__ == '__main__':
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    plusMinus(arr)
