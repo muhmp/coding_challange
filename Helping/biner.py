61d9ba0387ae6bdadc4522976937a2267d437d09
muhmp <defcoolz@gmail.com>
2022-04-08 22:07:39 +0900

    Update biner.py

--- Helping/biner.py
+++ Helping/biner.py
@@ -3,9 +3,9 @@
 # creating image A based on threshold T. the size of image is M X N.
 
 def biner(imgA, imgB, T, M, N):
-    for i in range(M-1):
-        for j in range(N-1):
-            if imgA[i][j] < T:
+    for i in range(0, M):
+        for j in range(0, N):
+            if imgA[i][j] <= T:
                 imgB[i][j] = 0
             else:
                 imgB[i][j] = 1
