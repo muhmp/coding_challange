1ad3ec5f6ca17634878870c40f4f7199a9bb221a
muhmp <defcoolz@gmail.com>
2022-12-07 20:41:34 +0900

    updates

--- LeetCode/introm.js
+++ LeetCode/introm.js
@@ -1,15 +1,32 @@
-/* integer to roman function */
 function intToRoman(num) {
-  var roman = "";
-  var romanNumeral = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
-  var numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-  for (var i = 0; i < numbers.length; i++) {
-    while (num >= numbers[i]) {
-      roman += romanNumeral[i];
-      num -= numbers[i];
+  // Create a lookup table for the Roman numerals
+  const lookup = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1
+  };
+  let roman = "";
+
+  // Loop through the lookup table and add the corresponding Roman numeral for each value
+  for (let i in lookup) {
+    while (num >= lookup[i]) {
+      roman += i;
+      num -= lookup[i];
     }
   }
+
   return roman;
 }
 
-console.log(intToRoman(1000))
+// Example usage
+console.log(intToRoman(10));  // Output: X
